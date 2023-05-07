956e7a285450bd2202c8f3ef1c712851bc17b6f8
maritzacaca <maritzacaca@student.telkomuniversity.ac.id>
2023-05-07 20:46:20 +0700

    ubah spacing

--- tcpserver.py
+++ tcpserver.py
@@ -2,12 +2,10 @@ from socket import *
 import sys
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-
 # Prepare a server socket
 serverPort = 80 # Port number for HTTP
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-
 while True:
     # Establish the connection
     print('Ready to serve...')
@@ -18,11 +16,9 @@ while True:
         f = open(filename[1:])
         outputdata = f.read()
         f.close()
-        
         # Send one HTTP header line into socket
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><title>HALO!!!</title><header><h1>TUBES JARKOM</h1></header></html>"
         connectionSocket.send(response.encode())
-
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].upper().encode())
@@ -34,6 +30,5 @@ while True:
         connectionSocket.send(response.encode())
         connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
         connectionSocket.close()
-
 serverSocket.close()
 sys.exit()
